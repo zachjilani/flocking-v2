59e76f0f05bbed3ae53fed780214c3b1c54b12cb
Michael <michaeljhart760@gmail.com>
2023-08-24 09:57:38 -0700

    test: update test cases to mocha and chai

--- vector3.test.js
+++ vector3.test.js
@@ -1,3 +1,4 @@
+const expect = require('chai').expect;
 const Vector3 = require('./vector3');
 
 describe('Vector3', () => {
@@ -5,54 +6,56 @@ describe('Vector3', () => {
     const vector1 = new Vector3(1, 2, 3);
     const vector2 = new Vector3(4, 5, 6);
     const result = vector1.add(vector2);
-    expect(result).toEqual(new Vector3(5, 7, 9));
+    expect(result).to.equal(new Vector3(5, 7, 9));
   });
 
   it('should subtract two vectors correctly', () => {
     const vector1 = new Vector3(1, 2, 3);
     const vector2 = new Vector3(4, 5, 6);
     const result = vector1.subtract(vector2);
-    expect(result).toEqual(new Vector3(-3, -3, -3));
+    expect(result).to.equal(new Vector3(-3, -3, -3));
   });
 
   it('should multiply two vectors correctly', () => {
     const vector1 = new Vector3(3, 4, 5);
     const vector2 = new Vector3(2, 2, 2);
     const result = vector1.mult(vector2);
-    expect(result).toEqual(new Vector3(6, 8, 10));
+    expect(result).to.equal(new Vector3(6, 8, 10));
   });
 
   it('should divide two vectors correctly', () => {
     const vector1 = new Vector3(4, 4, 6);
     const vector2 = new Vector3(2, 2, 2);
     const result = vector1.div(vector2);
-    expect(result).toEqual(new Vector3(2, 2, 3));
+    expect(result).to.equal(new Vector3(2, 2, 3));
   });
 
   //TODO: probably should limit digits after decimal points
   it('should calculate vectors magnitude correctly', () => {
     const vector1 = new Vector3(2, 3, 4);
     const result = vector1.magnitude()
-    expect(result).toEqual(5.385164807134504);
+    expect(result).should.be.closeTo(5.384, 0.001);
   });
 
   it('should normalize vectors correctly', () => {
     const vector1 = new Vector3(2, 3, 4);
     const result = vector1.normalize();
-    expect(result).toEqual(new Vector3(0.3713906763541037,0.5570860145311556,0.7427813527082074));
+    expect(result.x).should.be.closeTo(0.371, 0.001);
+    expect(result.y).should.be.closeTo(0.557, 0.001);
+    expect(result.z).should.be.closeTo(0.742, 0.001);
   });
 
   it('should calculate distance between two vectors correctly', () => {
     const vector1 = new Vector3(2, 3, 4);
     const vector2 = new Vector3(1, 2, 3);
     const result = vector1.distance(vector2);
-    expect(result).toEqual(1.7320508075688772);
+    expect(result).should.be.closeTo(1.732, 0.001);
   });
 
 
   it('should limit vector magnitude correctly', () => {
     const vector = new Vector3(3, 4, 5);
     const limited = vector.limit(4);
-    expect(limited.magnitude()).toBeLessThanOrEqual(4);
+    expect(limited.magnitude()).to.be.lessThanOrEqual(4);
   });
 });
